efe0c86bccb02823d543664feb2c8a063464b519
Pontus Persson <pom@skurt.com>
2017-07-13 16:17:01 +0200

    Remov unused traits.

--- src/index.js
+++ src/index.js
@@ -5,9 +5,7 @@ module.exports = {
     Elevator: require('./Elevator'),
     Fallaway: require('./Fallaway'),
     Headlight: require('./Headlight'),
-    Pickupable: require('./Pickupable'),
     Stun: require('./Stun'),
     Teleport: require('./Teleport'),
-    Translating: require('./Translating'),
     Weapon: require('./Weapon'),
 };
